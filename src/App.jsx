3af38df57d8b4a2c80285e1cca2f0a20de325b02
DEZester <aromudez@gmail.com>
2023-07-22 20:57:30 +0300

    Added toTop button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import styles from './style.js'
-
 import { Billing, Business, CardDeal, Clients, CTA, Footer, Hero, Navbar, Stats, Testimonials } from './components'
+import upArrowImg from './assets/uparrow.png'
+import { useEffect, useState } from 'react'
 
-const App = () =>
-	<div className='bg-primary w-full overflow-hidden'>
+const App = () => {
+	const [showTopButton, setShowTopButton] = useState(false)
+
+	const handleScrollToTopEvent = () => {
+		window.scrollTo({ top: 0, behavior: 'smooth' })
+	}
+
+	useEffect(() => {
+		const handleTopEvent = () => {
+			window.pageYOffset > 300 ? setShowTopButton(true) : setShowTopButton(false)
+		}
+
+		window.addEventListener('scroll', handleTopEvent)
+
+
+		return () => {
+			window.removeEventListener('scroll', handleTopEvent)
+		}
+	}, [])
+
+	return <div className='bg-primary w-full overflow-hidden'>
 		<div className={`${styles.paddingX} ${styles.flexCenter}`}>
 			<div className={`${styles.boxWidth}`}>
 				<Navbar />
@@ -26,10 +46,21 @@ const App = () =>
 				<Clients />
 				<CTA />
 				<Footer />
+
+				{showTopButton && (
+					<div>
+						<button
+							className=' bg-blue-gradient rounded-full fixed bottom-8 right-8 z-50 cursor-pointer p-2.5'
+							onClick={handleScrollToTopEvent}>
+							<img className='w-[30px] h-[30px]' src={upArrowImg} alt='up' />
+						</button>
+					</div>
+				)}
 			</div>
 		</div>
 
 
 	</div>
+}
 
 export default App
